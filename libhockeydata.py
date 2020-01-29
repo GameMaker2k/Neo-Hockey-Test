d96b5e6a1a9c2f2ede2a0ebc955a0845cd7b7ddf
Kazuki Suzuki Przyborowski <kazuki.przyborowski@gmail.com>
2020-01-29 06:18:24 -0600

    small update

--- libhockeydata.py
+++ libhockeydata.py
@@ -36,7 +36,7 @@ __version_info__ = (0, 1, 0, "RC 1", 1);
 __version_date_info__ = (2020, 1, 27, "RC 1", 1);
 __version_date__ = str(__version_date_info__[0])+"."+str(__version_date_info__[1]).zfill(2)+"."+str(__version_date_info__[2]).zfill(2);
 __revision__ = __version_info__[3];
-__revision_id__ = "$Id: a343838679e2df10642236ff90f6da14447244f7 $";
+__revision_id__ = "$Id$";
 if(__version_info__[4] is not None):
  __version_date_plusrc__ = __version_date__+"-"+str(__version_date_info__[4]);
 if(__version_info__[4] is None):
