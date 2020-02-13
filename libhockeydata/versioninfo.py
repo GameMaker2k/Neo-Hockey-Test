5083e8b829a42174bfeee8df452b85c526ead996
Kazuki Suzuki Przyborowski <kazuki.przyborowski@gmail.com>
2020-02-13 03:21:33 -0600

    Update versioninfo.py

--- libhockeydata/versioninfo.py
+++ libhockeydata/versioninfo.py
@@ -13,17 +13,17 @@
     Copyright 2015-2020 Game Maker 2k - https://github.com/GameMaker2k
     Copyright 2015-2020 Kazuki Przyborowski - https://github.com/KazukiPrzyborowski
 
-    $FileInfo: versioninfo.py - Last Update: 2/9/2020 Ver. 0.2.0 RC 1 - Author: cooldude2k $
+    $FileInfo: versioninfo.py - Last Update: 2/13/2020 Ver. 0.2.1 RC 1 - Author: cooldude2k $
 '''
 
 __program_name__ = "PyHockeyStats";
 __project__ = __program_name__;
 __project_url__ = "https://github.com/GameMaker2k/Neo-Hockey-Test";
-__version_info__ = (0, 2, 0, "RC 1", 1);
-__version_date_info__ = (2020, 2, 9, "RC 1", 1);
+__version_info__ = (0, 2, 1, "RC 1", 1);
+__version_date_info__ = (2020, 2, 13, "RC 1", 1);
 __version_date__ = str(__version_date_info__[0])+"."+str(__version_date_info__[1]).zfill(2)+"."+str(__version_date_info__[2]).zfill(2);
 __revision__ = __version_info__[3];
-__revision_id__ = "$Id$";
+__revision_id__ = "$Id:$";
 if(__version_info__[4] is not None):
  __version_date_plusrc__ = __version_date__+"-"+str(__version_date_info__[4]);
 if(__version_info__[4] is None):
