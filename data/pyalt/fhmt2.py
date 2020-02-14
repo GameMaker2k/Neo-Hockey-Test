fbcb4e8d1ce55291eaf9d8121254e1d268e6add6
Kazuki Suzuki Przyborowski <kazuki.przyborowski@gmail.com>
2020-02-13 19:11:40 -0600

    Adding more data formats

---
+++ data/pyalt/fhmt2.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from __future__ import absolute_import, division, print_function, unicode_literals;
+import libhockeydata.hockeyfunctions;
+
+hockeyarray = libhockeydata.hockeyfunctions.CreateHockeyArray("./fhmt2y17-18.db3");
+hockeyarray = libhockeydata.hockeyfunctions.AddHockeyLeagueToArray(hockeyarray, "HOL", "Hockey League", "USA", "United States", "20171001", "Division=1,Conference=1", "ORDER BY Points DESC, GamesPlayed ASC, TWins DESC, Losses ASC, GoalsDifference DESC", True, True);
+hockeyarray = libhockeydata.hockeyfunctions.AddHockeyConferenceToArray(hockeyarray, "HOL", "Conference 1");
+hockeyarray = libhockeydata.hockeyfunctions.AddHockeyDivisionToArray(hockeyarray, "HOL", "Division 1", "Conference 1");
+hockeyarray = libhockeydata.hockeyfunctions.AddHockeyTeamToArray(hockeyarray, "HOL", "Detroit", "MI", "USA", "United States", "Michigan", "Blasters", "Conference 1", "Division 1", "DET Arena", "Detroit", "");
+hockeyarray = libhockeydata.hockeyfunctions.AddHockeyTeamToArray(hockeyarray, "HOL", "Indianapolis", "IN", "USA", "United States", "Indiana", "Sabres", "Conference 1", "Division 1", "IND Arena", "Indianapolis", "");
+hockeyarray = libhockeydata.hockeyfunctions.AddHockeyDivisionToArray(hockeyarray, "HOL", "Division 2", "Conference 1");
+hockeyarray = libhockeydata.hockeyfunctions.AddHockeyTeamToArray(hockeyarray, "HOL", "Chicago", "IL", "USA", "United States", "Illionis", "River Bandits", "Conference 1", "Division 2", "CHI Arena", "Chicago", "");
+hockeyarray = libhockeydata.hockeyfunctions.AddHockeyTeamToArray(hockeyarray, "HOL", "Kansas City", "MO", "USA", "United States", "Missouri", "Chariots", "Conference 1", "Division 2", "KAN Arena", "Kansas City", "");
+hockeyarray = libhockeydata.hockeyfunctions.AddHockeyConferenceToArray(hockeyarray, "HOL", "Conference 2");
+hockeyarray = libhockeydata.hockeyfunctions.AddHockeyDivisionToArray(hockeyarray, "HOL", "Division 3", "Conference 2");
+hockeyarray = libhockeydata.hockeyfunctions.AddHockeyTeamToArray(hockeyarray, "HOL", "St. Paul", "MN", "USA", "United States", "Minnesota", "Smoking Guns", "Conference 2", "Division 3", "SMO Arena", "St. Paul", "");
+hockeyarray = libhockeydata.hockeyfunctions.AddHockeyTeamToArray(hockeyarray, "HOL", "Minneapolis", "MN", "USA", "United States", "Minnesota", "Armada", "Conference 2", "Division 3", "MIN Arena", "Minneapolis", "");
+hockeyarray = libhockeydata.hockeyfunctions.AddHockeyDivisionToArray(hockeyarray, "HOL", "Division 4", "Conference 2");
+hockeyarray = libhockeydata.hockeyfunctions.AddHockeyTeamToArray(hockeyarray, "HOL", "Peoria", "IL", "USA", "United States", "Illionis", "Jammers", "Conference 2", "Division 4", "PEO Arena", "Peoria", "");
+hockeyarray = libhockeydata.hockeyfunctions.AddHockeyTeamToArray(hockeyarray, "HOL", "Joliet", "IL", "USA", "United States", "Illionis", "Gears", "Conference 2", "Division 4", "JOL Arena", "Chicago", "");
+hockeyarray = libhockeydata.hockeyfunctions.AddHockeyLeagueToArray(hockeyarray, "MIL", "Minor League", "USA", "United States", "20171001", "Division=1,Conference=1", "ORDER BY PCT DESC, GamesPlayed ASC, TWins DESC, Losses ASC, GoalsDifference DESC", True, True);
+hockeyarray = libhockeydata.hockeyfunctions.AddHockeyConferenceToArray(hockeyarray, "MIL", "Conference 1");
+hockeyarray = libhockeydata.hockeyfunctions.AddHockeyDivisionToArray(hockeyarray, "MIL", "Division 1", "Conference 1");
+hockeyarray = libhockeydata.hockeyfunctions.AddHockeyTeamToArray(hockeyarray, "MIL", "Northview", "MI", "USA", "United States", "Michigan", "Loggerheads", "Conference 1", "Division 1", "NOR Arena", "Northview", "");
+hockeyarray = libhockeydata.hockeyfunctions.AddHockeyTeamToArray(hockeyarray, "MIL", "Gulivoire Park", "IN", "USA", "United States", "Indiana", "Caravans", "Conference 1", "Division 1", "GUL Arena", "Gulivoire Park", "");
+hockeyarray = libhockeydata.hockeyfunctions.AddHockeyDivisionToArray(hockeyarray, "MIL", "Division 2", "Conference 1");
+hockeyarray = libhockeydata.hockeyfunctions.AddHockeyTeamToArray(hockeyarray, "MIL", "Wheeling", "IL", "USA", "United States", "Illionis", "Ramblers", "Conference 1", "Division 2", "WHE Arena", "Wheeling", "");
+hockeyarray = libhockeydata.hockeyfunctions.AddHockeyTeamToArray(hockeyarray, "MIL", "Columbia", "MO", "USA", "United States", "Missouri", "Skychiefs", "Conference 1", "Division 2", "COL Arena", "Columbia", "");
+hockeyarray = libhockeydata.hockeyfunctions.AddHockeyConferenceToArray(hockeyarray, "MIL", "Conference 2");
+hockeyarray = libhockeydata.hockeyfunctions.AddHockeyDivisionToArray(hockeyarray, "MIL", "Division 3", "Conference 2");
+hockeyarray = libhockeydata.hockeyfunctions.AddHockeyTeamToArray(hockeyarray, "MIL", "Winona", "MN", "USA", "United States", "Minnesota", "Vipers", "Conference 2", "Division 3", "WIN Arena", "Winona", "");
+hockeyarray = libhockeydata.hockeyfunctions.AddHockeyTeamToArray(hockeyarray, "MIL", "New Hope", "MN", "USA", "United States", "Minnesota", "Chaos", "Conference 2", "Division 3", "NEW Arena", "New Hope", "");
+hockeyarray = libhockeydata.hockeyfunctions.AddHockeyDivisionToArray(hockeyarray, "MIL", "Division 4", "Conference 2");
+hockeyarray = libhockeydata.hockeyfunctions.AddHockeyTeamToArray(hockeyarray, "MIL", "Tinley Park", "IL", "USA", "United States", "Illionis", "Gappers", "Conference 2", "Division 4", "TIN Arena", "Tinley Park", "");
+hockeyarray = libhockeydata.hockeyfunctions.AddHockeyTeamToArray(hockeyarray, "MIL", "Franklin Park", "IL", "USA", "United States", "Illionis", "Spirits", "Conference 2", "Division 4", "FRA Arena", "Franklin Park", "");
+
+libhockeydata.hockeyfunctions.MakeHockeyDatabaseFromHockeyArray(hockeyarray, None, False, False, False);
