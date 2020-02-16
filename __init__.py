fe77bca4b4a35626243e57230b0864539339e887
Kazuki Suzuki Przyborowski <kazuki.przyborowski@gmail.com>
2020-02-16 02:37:13 -0600

    Add files via upload

---
+++ __init__.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+'''
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the Revised BSD License.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    Revised BSD License for more details.
+
+    Copyright 2015-2020 Game Maker 2k - https://github.com/GameMaker2k
+    Copyright 2015-2020 Kazuki Przyborowski - https://github.com/KazukiPrzyborowski
+
+    $FileInfo: __init__.py - Last Update: 2/9/2020 Ver. 0.2.0 RC 1 - Author: cooldude2k $
+'''
+
+from __future__ import absolute_import, division, print_function, unicode_literals;
+from libhockeydata.hockeyshortcuts import *;
+from libhockeydata.versioninfo import __program_name__, __project__, __project_url__, __version__, __version_date__, __version_info__, __version_date_info__, __version_date__, __revision__, __revision_id__, __version_date_plusrc__;
