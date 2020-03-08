35fb7796cb8e99374c4fdd13286066d93d52704a
Kazuki Suzuki Przyborowski <kazuki.przyborowski@gmail.com>
2020-03-08 05:14:33 -0500

    Add files via upload

---
+++ pyverinfo.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+'''
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the Revised BSD License.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    Revised BSD License for more details.
+
+    Copyright 2011-2020 Game Maker 2k - https://github.com/GameMaker2k
+    Copyright 2011-2020 Kazuki Przyborowski - https://github.com/KazukiPrzyborowski
+
+    $FileInfo: pyverinfo.py - Last Update: 12/3/2019 Ver. 2.7.19 RC 1 - Author: cooldude2k $
+'''
+
+import re, os, sys, json, subprocess;
+
+pyexecpath = os.path.realpath(sys.executable);
+pkgsetuppy = os.path.realpath("."+os.path.sep+"setup.py");
+pypkgenlistp = subprocess.Popen([pyexecpath, pkgsetuppy, "getversioninfo"], stdout=subprocess.PIPE, stderr=subprocess.PIPE);
+pypkgenout, pypkgenerr = pypkgenlistp.communicate();
+if(sys.version[0]=="3"):
+ pypkgenout = pypkgenout.decode('utf-8');
+pyconfiginfo = json.loads(pypkgenout);
+print(pypkgenout);
