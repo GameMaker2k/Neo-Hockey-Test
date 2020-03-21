a699662f4d100c849f7349107721f7d13fd16ca2
Kazuki Suzuki Przyborowski <kazuki.przyborowski@gmail.com>
2020-03-21 04:13:39 -0500

    Update versioninfo.py

--- libhockeydata/versioninfo.py
+++ libhockeydata/versioninfo.py
@@ -13,7 +13,7 @@
     Copyright 2015-2020 Game Maker 2k - https://github.com/GameMaker2k
     Copyright 2015-2020 Kazuki Przyborowski - https://github.com/KazukiPrzyborowski
 
-    $FileInfo: versioninfo.py - Last Update: 2/17/2020 Ver. 0.3.1 RC 1 - Author: cooldude2k $
+    $FileInfo: versioninfo.py - Last Update: 3/21/2020 Ver. 0.3.9 RC 1 - Author: cooldude2k $
 '''
 
 from __future__ import absolute_import, division, print_function, unicode_literals;
@@ -70,7 +70,7 @@ __status__ = "Production";
 __program_name__ = "PyHockeyStats";
 __project__ = __program_name__;
 __project_url__ = "https://github.com/GameMaker2k/Neo-Hockey-Test";
-__version_info__ = (0, 3, 1, "RC 1", 1);
+__version_info__ = (0, 3, 9, "RC 1", 1);
 __revision__ = __version_info__[3];
 __revision_id__ = "$Id$";
 if(__version_info__[3] is not None):
@@ -83,7 +83,7 @@ def version_info():
   return {"major": __version_info__[0], "minor": __version_info__[1], "build": __version_info__[2], "release": __version_info__[3]};
  if(__version_info__[3] is None):
   return {"major": __version_info__[0], "minor": __version_info__[1], "build": __version_info__[2], "release": None};
-__version_date_info__ = (2020, 2, 26, "RC 1", 1);
+__version_date_info__ = (2020, 3, 21, "RC 1", 1);
 def version_date():
  if(__version_date_info__[3] is not None):
   return {"year":__version_date_info__[0], "month": __version_date_info__[1], "day": __version_date_info__[2], "release": __version_date_info__[3]};
