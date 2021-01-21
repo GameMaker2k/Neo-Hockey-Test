835e9b9bc02296469f09b3986b2266b33d153a6d
Kazuki Suzuki Przyborowski <kazuki.przyborowski@gmail.com>
2021-01-21 06:29:37 -0600

    Update versioninfo.py

--- libhockeydata/versioninfo.py
+++ libhockeydata/versioninfo.py
@@ -72,7 +72,7 @@ __project__ = __program_name__;
 __project_url__ = "https://github.com/GameMaker2k/Neo-Hockey-Test";
 __version_info__ = (0, 5, 0, "RC 1", 1);
 __revision__ = __version_info__[3];
-__revision_id__ = "$Id$";
+__revision_id__ = "$Id:$";
 if(__version_info__[3] is not None):
  __version__ = "{major}.{minor}.{build} {release}".format(major=__version_info__[0], minor=__version_info__[1], build=__version_info__[2], release=__version_info__[3]);
 if(__version_info__[3] is None):
