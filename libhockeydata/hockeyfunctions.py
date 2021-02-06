765d06ff3abd576b938788070ec3e54f73a9ba07
Kazuki Suzuki Przyborowski <kazuki.przyborowski@gmail.com>
2021-02-06 02:17:06 -0600

    Update hockeyfunctions.py

--- libhockeydata/hockeyfunctions.py
+++ libhockeydata/hockeyfunctions.py
@@ -17,7 +17,8 @@
 '''
 
 from __future__ import absolute_import, division, print_function, unicode_literals;
-import sqlite3, sys, os, re, time, json, ftplib, pickle, marshal, platform, binascii, xml.dom.minidom;
+import sqlite3, sys, os, re, time, json, pickle, marshal, platform, binascii, xml.dom.minidom;
+from ftplib import FTP, FTP_TLS;
 
 try:
  import xml.etree.cElementTree as cElementTree;
