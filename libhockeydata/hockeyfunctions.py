3a54a72df0171e75bb6492bd99143f6f2222dc97
Kazuki Suzuki Przyborowski <kazuki.przyborowski@gmail.com>
2021-02-06 02:15:32 -0600

    Update hockeyfunctions.py

--- libhockeydata/hockeyfunctions.py
+++ libhockeydata/hockeyfunctions.py
@@ -24,6 +24,11 @@ try:
 except ImportError:
  import xml.etree.ElementTree as cElementTree;
 
+try:
+ from urlparse import urlparse;
+except ImportError:
+ from urllib.parse import urlparse;
+
 from .hockeydatabase import *;
 from .hockeydwnload import *;
 from .versioninfo import __program_name__, __project__, __project_url__, __version__, __version_alt__, __version_date__, __version_date_alt__, __version_info__, __version_date_info__, __version_date__, __revision__, __revision_id__, __version_date_plusrc__;
