ca4f7f75d8d7df8d3c996d2ca4b9d376974e5ddf
Kazuki Suzuki Przyborowski <kazuki.przyborowski@gmail.com>
2021-02-11 02:02:35 -0600

    Update example.py

--- example.py
+++ example.py
@@ -34,7 +34,7 @@ if(os.path.isdir(rootdir)):
   print("");
   for file in files:
    ext = os.path.splitext(file)[-1].lower();
-   if(ext=="gz" or ext=="bz2" or ext=="lzma" or ext=="xz"):
+   if(ext==".gz" or ext==".bz2" or ext==".lzma" or ext==".xz"):
     ext = os.path.splitext(ext)[-1].lower();
    if ext in extensions:
     filepath = os.path.join(subdir, file);
