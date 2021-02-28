bb4ca3aff20936fc087308dca64f90b732b190ee
Kazuki Suzuki Przyborowski <kazuki.przyborowski@gmail.com>
2021-02-28 13:10:16 -0600

    Update hockeyfunctions.py

--- libhockeydata/hockeyfunctions.py
+++ libhockeydata/hockeyfunctions.py
@@ -741,7 +741,7 @@ def MakeHockeyJSONFromHockeyArray(inhockeyarray, jsonindent=1, verbose=True, jso
  if(verbose and jsonverbose):
   VerbosePrintOut(jsonstring);
  elif(verbose and not jsonverbose):
-  VerbosePrintOut(MakeHockeyXMLFromHockeyArray(MakeHockeyArrayFromHockeyJSON(inhockeyarray, verbose=False, jsonverbose=True), verbose=False, jsonverbose=False));
+  VerbosePrintOut(MakeHockeyXMLFromHockeyArray(inhockeyarray, verbose=False, jsonverbose=True), verbose=False, jsonverbose=False);
  return jsonstring;
 
 def MakeHockeyJSONFileFromHockeyArray(inhockeyarray, outjsonfile=None, returnjson=False, jsonindent=1, verbose=True, jsonverbose=True):
