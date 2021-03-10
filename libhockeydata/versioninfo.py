9f9a2f747e79d6a774c5c053a1bb15a0d8032546
Kazuki Suzuki Przyborowski <kazuki.przyborowski@gmail.com>
2021-03-10 01:14:47 -0600

    Update versioninfo.py

--- libhockeydata/versioninfo.py
+++ libhockeydata/versioninfo.py
@@ -113,7 +113,7 @@ def version_info():
   return {"major": __version_info__[0], "minor": __version_info__[1], "build": __version_info__[2], "release": __version_info__[3]};
  if(__version_info__[3] is None):
   return {"major": __version_info__[0], "minor": __version_info__[1], "build": __version_info__[2], "release": None};
-__version_date_info__ = (2021, 1, 15, "RC 1", 1);
+__version_date_info__ = (2021, 3, 10, "RC 1", 1);
 def version_date():
  if(__version_date_info__[3] is not None):
   return {"year":__version_date_info__[0], "month": __version_date_info__[1], "day": __version_date_info__[2], "release": __version_date_info__[3]};
