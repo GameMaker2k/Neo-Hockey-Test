16fbe76f4072e65bc69a861b462ad346ff66503c
Kazuki Suzuki Przyborowski <kazuki.przyborowski@gmail.com>
2024-11-17 23:43:51 -0600

    Add files via upload

--- pyhockeystats/sgmldtd/__init__.py
+++ pyhockeystats/sgmldtd/__init__.py
@@ -19,6 +19,7 @@
 from __future__ import absolute_import, division, print_function, unicode_literals, generators, with_statement, nested_scopes
 
 import os
+from io import open
 
 implib = False
 pkgres = False
